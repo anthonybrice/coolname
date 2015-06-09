fa0d82c0b7462b39f854290217b1fc821f803ecc
Anthony Brice <anthonybrice@lateachiever.com>
2015-06-08 21:41:12 -0700

    updates

--- import/import.js
+++ import/import.js
@@ -1,8 +1,21 @@
 /**
  * File: import.js
- * Desc: A program that reads and imports media files into nodejs
+ * Desc: A program that reads and imports media files into a mongodb instance.
  */
 
-var fs = require('fs')
-  , mm = require('musicmetadata')
-
+var fs = require("fs")
+  , path = require("path")
+  , mm = require("musicmetadata")
+  , mdb = require("mongodb")
+  , argv = require("argp")
+             .createParser( { once: true } )
+             .description("A program that reads and imports media"
+                          + " into a mongodb instance.")
+             .email("anthonybrice@latechiever.com")
+             .body()
+             .text(" Arguments:")
+             .argument("FILE", { description: "Filepath at which to begin.\n"
+                                              + "Defaults to current directory."
+                               } )
+             .help()
+             .argv()
