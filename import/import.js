f8377498a2876a1c24089a66eccc931f5216ac37
Anthony Brice <anthonybrice@lateachiever.com>
2015-06-08 23:54:22 -0700

    updates

--- import/import.js
+++ import/import.js
@@ -1,21 +1,32 @@
 /**
  * File: import/import.js
- * Desc: A program that reads and imports media files into nodejs
+ * Desc: A program that reads and imports media files into mongodb
  */
+
+"use strict"
 
 var fs = require("fs")
   , path = require("path")
   , mm = require("musicmetadata")
   , mdb = require("mongodb")
-  , argv = require("argp")
-             .createParser( { once: true } )
-             .description("A program that reads and imports media"
-                          + " into a mongodb instance.")
-             .email("anthonybrice@latechiever.com")
-             .body()
-             .text(" Arguments:")
-             .argument("FILE", { description: "Filepath at which to begin.\n"
-                                              + "Defaults to current directory."
-                               } )
-             .help()
-             .argv()
+  , argv = require('minimist')(process.argv.slice(2))
+
+var dir = argv._.length >= 1
+        ? argv._[0]
+        : process.cwd()
+/**
+ * A callback for fs.readdir(). Adds MP3 and FLAC files to database and recurses
+ * directories.
+ */
+function handleDirectory(err, files) {
+  files.forEach(function (file) {
+    if (file.isDirectory()) {
+      fs.readdir(file, handleDirectory)
+      return
+    }
+
+
+  })
+}
+
+fs.readdir(dir, handleDirectory)
