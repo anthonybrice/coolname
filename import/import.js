9b2d2bd682a0d0311d6321ef16c4b5a2bb6e0330
Anthony Brice <anthonybrice@lateachiever.com>
2015-06-09 00:34:19 -0700

    updates

--- import/import.js
+++ import/import.js
@@ -14,18 +14,26 @@ var fs = require("fs")
 var dir = argv._.length >= 1
         ? argv._[0]
         : process.cwd()
+
+function handleMedia(file) {
+
+}
+
 /**
  * A callback for fs.readdir(). Adds MP3 and FLAC files to database and recurses
  * directories.
  */
 function handleDirectory(err, files) {
   files.forEach(function (file) {
-    if (file.isDirectory()) {
-      fs.readdir(file, handleDirectory)
-      return
-    }
-
-
+    fs.lstat(file, function (err, stats) {
+      if (stats.isDirectory()) {
+        fs.readdir(file, handleDirectory)
+      } else if (stats.isFile()
+                && (file.endsWith("mp3")
+                   || file.endsWith("flac"))) {
+        handleMedia(file)
+      }
+    })
   })
 }
 
