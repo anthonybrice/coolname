7c5a342faf92b90b80ab7f487a3cfe478d123afd
Anthony Brice <anthonybrice@lateachiever.com>
2015-06-09 15:05:51 -0700

    recurses directories

--- import/import.js
+++ import/import.js
@@ -5,52 +5,31 @@
 
 "use strict"
 
-var fs = require("fs")
-  , path = require("path")
+var dir = require("node-dir")
   , mm = require("musicmetadata")
   , mdb = require("mongodb")
   , parseArgs = require("minimist")
 
-var argv = parseArgs(process.argv.slice(2),
-                    { "boolean": [ "d", "debug" ] } )
+var argv = parseArgs( process.argv.slice(2)
+                    , { "boolean": [ "d", "debug" ] }
+                    )
 
 var debug = argv["d"] !== undefined || argv["debug"] !== undefined
 if (debug) console.log("Debug enabled.")
 
-var dir = argv._.length >= 1
-        ? argv._[0]
-        : process.cwd()
+var dirArg = argv._.length >= 1
+           ? argv._[0]
+           : process.cwd()
 
 function handleMedia(file) {
   if (debug) console.log(file + " is media")
 }
 
-/**
- * A callback for fs.readdir(). Adds MP3 and FLAC files to database and recurses
- * directories.
- */
-function handleDirectory(err, files) {
-  if (err) {
-    console.log(err)
-    process.exit()
-  }
-  files.forEach(function (file) {
-    fs.lstat(file, function (err, stats) {
-      if (err) return
-      if (stats.isDirectory()) {
-        if (debug) console.log(file + " is a directory")
-        fs.readdir(file, handleDirectory)
-        return
-      } else if (stats.isFile()) {
-        var re = /(?:\.([^.]+))?$/
-          , ext = re.exec(file)
-        if (ext === "mp3" || ext === "flac") handleMedia(file)
-      }
-    })
-  })
+if (debug) {
+  console.log("Beginning recursive descent from " + dirArg)
 }
 
-if (debug) {
-  console.log("Beginning recursive descent from " + dir)
-}
-fs.readdir(dir, handleDirectory)
+dir.files(dirArg, function (err, files) {
+  if (err) throw err
+  if (debug) console.log(files)
+})
