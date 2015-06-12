04356021f9a9de42331a4ecc778608a43a285181
Anthony Brice <anthonybrice@lateachiever.com>
2015-06-11 21:01:26 -0700

    Closures save me from global scope

--- import/import.js
+++ import/import.js
@@ -3,7 +3,8 @@
  *
  * Desc: A program that reads and imports media files into mongodb. Space
  * requirement scales linearly on the number of files below the passed
- * directory, but it's just to make an array of the filenames.
+ * directory, but that's only to make an array of all the filenames. Hopefully
+ * that's reasonable.
  */
 
 "use strict"
@@ -20,13 +21,16 @@ var assert = require("assert")
 // Main //
 //////////
 
-console.time("import")
 var argv = parseArgs( process.argv.slice(2)
-                    , { "boolean": [ "d", "debug" ] }
+                    , { "boolean": [ "d", "debug" ]
+                      , "time": [ "t", "time" ]
+                      }
                     )
 
 var debug = argv["d"] !== false || argv["debug"] !== false
+  , time = argv["t"] !== false || argv["time"] !== false
 if (debug) console.log("Debug enabled.")
+if (time) console.time("exec")
 
 var url = "mongodb://localhost"
   , db
@@ -49,60 +53,47 @@ function main() {
     console.log("Beginning recursive descent from " + dirArg)
   }
 
-  dir.files(dirArg, handleFiles)
+  dir.files(dirArg, handleFiles())
 }
 
 ///////////////
 // Functions //
 ///////////////
 
-/**
- * The number of files we expect to insert.
- */
-var numFiles
+function handleFiles() {
+  var numFiles
+    , currNum = 0
 
-/**
- * The current number of files inserted. handleMedia() uses this to determine if
- * it should close the connections or not. This seems to me a pretty ugly
- * solution, but I see no other way while keeping synchronous filesystem access,
- * which is essential to a decent exec time.
- */
-var currNum = 0
-
-/**
- * Iterates through `files` and adds each MP3 and FLAC file
- * to a mongodb database.
- */
-function handleFiles(err, files) {
-  assert.equal(null, err)
-  numFiles = files.length
-  files.forEach(function (file) {
-    fs.lstat(file, function (err, stats) {
-      assert.equal(null, err)
-      if (!stats.isFile()) return
-      var ext = path.extname(file)
-      if (ext === ".mp3" || ext === ".flac") handleMedia(file)
-    })
-  })
-}
-
-/**
- * Adds `file` to a mongodb instance.
- */
-function handleMedia(file) {
-  var parser = mm(fs.createReadStream(file), function (err, metadata) {
+  function fileIterator(err, files) {
     assert.equal(null, err)
-    metadata.filepath = file
-    if (debug) console.log(metadata)
-    coll.insertOne(metadata, function (err, r) {
-      assert.equal(null, err)
-      assert.equal(1, r.insertedCount)
-      if (debug) console.log("In callback")
-      currNum++
-      if (currNum === numFiles) {
-        console.timeEnd("import")
-        db.close()
-      }
+    numFiles = files.length
+    files.forEach(function (file) {
+      fs.lstat(file, function (err, stats) {
+        assert.equal(null, err)
+        if (!stats.isFile()) return
+        var ext = path.extname(file)
+        if (ext === ".mp3" || ext === ".flac") handleMedia(file)
+      })
     })
-  })
+  }
+
+  function handleMedia(file) {
+    var parser = mm(fs.createReadStream(file), function (err, metadata) {
+      assert.equal(null, err)
+      metadata.filepath = file
+      if (debug) console.log(metadata)
+      coll.insertOne(metadata, function (err, r) {
+        assert.equal(null, err)
+        assert.equal(1, r.insertedCount)
+        if (debug) console.log("In callback")
+        currNum++
+        if (currNum === numFiles) {
+          if (time) console.timeEnd("exec")
+          db.close()
+        }
+      })
+    })
+  }
+
+  return fileIterator
 }
