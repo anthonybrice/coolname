437bf544f36ebd35585dfccd43b41db1ca76b3cf
Anthony Brice <anthonybrice@lateachiever.com>
2015-07-14 19:14:28 -0700

    ffff

--- app/player/player.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  "use strict"
-  var app = angular.module('player', [])
-  app.controller('PlayerController', function () {
-    this.desc = "The player controller"
-  })
-})()
---
+++ app/player/playerDirective.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict"
+
+  angular
+    .module("player")
+    .directive("playerDirective", playerDirective)
+
+  function playerDirective() {
+
+  }
+})()
